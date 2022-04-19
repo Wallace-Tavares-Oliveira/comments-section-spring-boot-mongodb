b4a10c1f82f3c3715646305e8031c6d67df6d02e
Wallace Tavares Oliveira <wallacetavaresoliveira84@gmail.com>
2022-04-19 20:13:11 -0300

    Poject created

---
+++ src/main/java/com/project/commentssection/CommentssectionApplication.java
@@ -0,0 +1,13 @@
+package com.project.commentssection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommentssectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommentssectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/commentssection/CommentssectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.commentssection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommentssectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
