b4805ccc5c2f970ec543268e46df203c0fb2e1bd
Wallace Tavares Oliveira <wallacetavaresoliveira84@gmail.com>
2022-04-20 19:23:40 -0300

    User update with PUT 71

--- src/main/java/com/project/commentssection/resources/UserResource.java
+++ src/main/java/com/project/commentssection/resources/UserResource.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -48,9 +49,17 @@ public class UserResource {
 		return ResponseEntity.created(uri).build();
 	}
 
-	@DeleteMapping(value = "/{id}") //Using DeleteMapping insted of RequestMapping
+	@DeleteMapping(value = "/{id}") // Using DeleteMapping insted of RequestMapping
 	public ResponseEntity<UserDTO> delete(@PathVariable String id) {
 		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id) {
+		User obj = service.fromDTO(objDto);
+		obj.setId(id);
+		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
 	}
 
