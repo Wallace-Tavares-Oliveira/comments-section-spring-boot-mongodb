f2de4fd15bb42d5f697514015a8c650760eca28e
RenanTelini <renan_telini@hotmail.com>
2022-04-20 15:15:40 -0300

    Getting user by id 68

--- src/main/java/com/project/commentssection/resources/UserResource.java
+++ src/main/java/com/project/commentssection/resources/UserResource.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,12 +21,18 @@ public class UserResource {
 	@Autowired
 	private UserService service;
 
-	// Refactored method to DTO  
+	// Refactored method to DTO
 	@GetMapping
-	public ResponseEntity <List<UserDTO>> findAll(){
-		List <User> list = service.findAll();
+	public ResponseEntity<List<UserDTO>> findAll() {
+		List<User> list = service.findAll();
 		List<UserDTO> lisDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(lisDto);
 	}
-	
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(new UserDTO(obj));
+	}
+
 }
