abd591eb8b3629b44d86a698220af72164df9844
Wallace Tavares Oliveira <wallacetavaresoliveira84@gmail.com>
2022-04-21 13:26:26 -0300

    Endpoint to return user posts 76

--- src/main/java/com/project/commentssection/resources/UserResource.java
+++ src/main/java/com/project/commentssection/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.project.commentssection.domain.Post;
 import com.project.commentssection.domain.User;
 import com.project.commentssection.dto.UserDTO;
 import com.project.commentssection.services.UserService;
@@ -61,6 +62,12 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping(value = "/{id}/posts")
+	public ResponseEntity<List<Post>> findPost(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 
 }
